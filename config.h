7d3f227a2605dd323fb86d5f9180903c76626fab
CronyAkatsuki <cronyakatsuki@gmail.com>
2021-05-22 18:00:40 +0200

    More fonts.

--- config.h
+++ config.h
@@ -11,7 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=10" };
+static const char *fonts[]          = { "JetBrains Mono:size=10:antialias=true:autohint=true",
+                                        "SauceCodePro Nerd Font Mono:size:12:antialias=true"};
 static const char dmenufont[]       = "JetBrains Mono:size=10";
 static const char col_gray1[]       = "#232121";
 static const char col_gray2[]       = "#44475a";
